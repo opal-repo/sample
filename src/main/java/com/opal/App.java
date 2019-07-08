470b23621ce9d5ff6f20b75e3b69092c70ba743e
sergeyk <sargeik@oft.co.il>
2019-07-08 10:55:14 +0300

    ergey first commit

--- src/main/java/com/opal/App.java
+++ src/main/java/com/opal/App.java
@@ -8,6 +8,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" + sergey());
     }
+
+    public static String sergey(){return new String(" Sergey");}
 }

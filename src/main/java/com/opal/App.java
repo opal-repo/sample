836405b111e5be216d3dc7e992e4ca6b86a9bd7e
danmorgen <45553267+danmorgen@users.noreply.github.com>
2019-07-08 11:04:43 +0300

    Revert "Features/feature 1"

--- src/main/java/com/opal/App.java
+++ src/main/java/com/opal/App.java
@@ -9,7 +9,5 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        //ira commit
-        //Commited from Vitaly to feature
     }
 }
